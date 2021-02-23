8c349ea750f3a7029b200285c6eb9c9c4ca1ef55
krysiakpawel <krysiak.p@o2.pl>
2021-02-23 17:59:18 +0000

    Exercise 6.2

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,36 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
 public class TestingMain {
     public static void main(String[] args){
-        System.out.println("Module 6. Introduction to program testing");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")){
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        Calculator calculator = new Calculator();
+
+        int addResult = calculator.add(5, 5);
+
+        if (addResult == 10){
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        int subtractResult = calculator.subtract(10, 5);
+
+        if (subtractResult == 5){
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
     }
 }
