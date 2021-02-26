17195ee33adf7e7d2b6e8d9e8655deea0f0e447f
krysiakpawel <krysiak.p@o2.pl>
2021-02-26 13:35:44 +0000

    Module 6.3

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,36 +1,8 @@
 package com.kodilla.testing;
-
-import com.kodilla.testing.calculator.Calculator;
-import com.kodilla.testing.user.SimpleUser;
+;
 
 public class TestingMain {
-    public static void main(String[] args){
-        SimpleUser simpleUser = new SimpleUser("theForumUser");
+    public static void main(String[] args) {
 
-        String result = simpleUser.getUsername();
-
-        if (result.equals("theForumUser")){
-            System.out.println("test OK");
-        } else {
-            System.out.println("Error!");
-        }
-
-        Calculator calculator = new Calculator();
-
-        int addResult = calculator.add(5, 5);
-
-        if (addResult == 10){
-            System.out.println("test OK");
-        } else {
-            System.out.println("Error!");
-        }
-
-        int subtractResult = calculator.subtract(10, 5);
-
-        if (subtractResult == 5){
-            System.out.println("test OK");
-        } else {
-            System.out.println("Error!");
-        }
     }
 }
